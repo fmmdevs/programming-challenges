1052797bd8d6b08b7187006734368a9e95cee24b
fmmdevs <fmm.devs@gmail.com>
2024-04-25 11:41:31 +0200

    4. Es Primo

---
+++ programming-challenges/003 Es Primo/script.js
@@ -0,0 +1,18 @@
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+
+const esPrimo = (n) => {
+
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+};
+
+for (let i = 1; i <= 100; i++) {
+    if (esPrimo(i)) {
+        console.log(i);
+    }
+}
